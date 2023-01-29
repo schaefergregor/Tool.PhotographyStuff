d603f4d1a4b8db9447f154143cc09fed0692d1b9
Gregor <g.schaefer1990@gmail.com>
2023-01-29 12:35:30 +0100

    Add description.

--- remove_movie_files.py
+++ remove_movie_files.py
@@ -4,13 +4,17 @@ Remove movie files in subdirectories
 import os
 
 # declare list of types
-extenstions = (".mp4", ".MOV")
+extensions = (".mp4", ".MOV")
 # get current path
 dir_path = cwd = os.getcwd()
-# get all subfolders
+# get all sub folders
 directories = os.listdir(dir_path)
 
+
 def remove_movie_files(directory, subdirectory):
+    """
+    Remove movie files from dir
+    """
     # declare directory
     movie_directory = os.path.join(directory, subdirectory)
     # check dir exists
@@ -18,7 +22,7 @@ def remove_movie_files(directory, subdirectory):
         return
 
     for file in os.listdir(movie_directory):
-        if file.endswith(extenstions):
+        if file.endswith(extensions):
             file_to_delete = os.path.join(movie_directory, file)
             print(file_to_delete)
             os.remove(file_to_delete)
@@ -27,7 +31,7 @@ def remove_movie_files(directory, subdirectory):
 for directory in directories:
     if directory.startswith('.'):
         continue
-    
+
     # remove camera movies
     remove_movie_files(os.path.join(dir_path, directory), "camera")
     # remove mobile movies
